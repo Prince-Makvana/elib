8ff11b8ce2b33fe91e7a46ba7a4b0cb0d8145da1
Prince-Makvana <princepatel2274@gmail.com>
2025-11-08 12:16:20 +0530

    database setup

---
+++ src/config/db.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { config } from "./config.ts";
+
+
+const connectDB = async () =>{
+    try{
+        mongoose.connection.on("connected", ()=>{
+            console.log("Connecte to database successfuly.");
+        })
+        
+        mongoose.connection.on('error', (err)=>{
+            console.log("Error in connecting to database.",err);
+        })
+        
+        await mongoose.connect(config.databaseUrl as string);
+
+    }catch(err){
+        console.error("Failed to connect to database.",err);
+        process.exit(1);
+    }
+
+}
+
+export default connectDB;
