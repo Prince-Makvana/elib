ffc2865c1581dd7f9b22faedf4e25b3097f1e213
Prince-Makvana <princepatel2274@gmail.com>
2025-11-11 18:11:20 +0530

    created user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { type NextFunction, type Request, type Response } from "express"
 import globalErrorHandler from "./middlewares/globalErrorHandler.ts";
+import userRouter from "./user/userRouter.ts";
 const app = express();
 
 app.get("/",(req, res, next) =>{
     res.json({message:"Hello World!"});
-})
+});
 
+app.use('/api/users',userRouter);
 
 app.use(globalErrorHandler);
 
