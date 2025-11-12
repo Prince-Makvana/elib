9c92b38dd12bfca657a254f3596ae691c76c377e
Prince-Makvana <princepatel2274@gmail.com>
2025-11-12 11:17:43 +0530

    add regiter validation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { type NextFunction, type Request, type Response } from "express
 import globalErrorHandler from "./middlewares/globalErrorHandler.ts";
 import userRouter from "./user/userRouter.ts";
 const app = express();
+app.use(express.json());
 
 app.get("/",(req, res, next) =>{
     res.json({message:"Hello World!"});
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,16 @@
 import type { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        const error = createHttpError(400, "All fields are required...");
+        return next(error);
+        // return res.json({message: "All fields are required..."})
+    }
+    
     res.json({message: "User Created..."})
 };
 
